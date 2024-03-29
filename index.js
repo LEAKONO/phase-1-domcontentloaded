4b50a10e80a9419b576f6fae92290140575db0f4
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-03-29 12:59:25 +0300

    dom element

--- index.js
+++ index.js
@@ -2,11 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     const paragraph = document.querySelector('p');
-    if (paragraph) {
-      paragraph.textContent = "This is really cool!";
-    }
-  });
-  
-
+    paragraph.textContent="This is really cool!";
+});
 
   
